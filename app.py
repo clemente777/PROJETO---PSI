3b993628bccb0e78b0def06561f35d280c11281b
clemente777 <brunecaclemente@gmail.com>
2025-07-10 10:01:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, session, render_template, request, flash, redirect, url_for
 from werkzeug.security import check_password_hash
+import sqlite3
 
+DATABASE = 'banco.sql'
+conn = sqlite3.connect('banco.db')
+with open(DATABASE) as f:
+    conn.executescript(f.read())
+conn.commit()
+conn.close()
 
 app = Flask(__name__)
 
